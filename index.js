9f0c3c1817949fca13b9376313cece8d51f0e768
s1rcody66 <cody200600@gmail.com>
2024-09-22 23:10:45 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'M ${age} years old, Happy birthday to me!`);
+}
+ 
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.legnth; i++) (
+        messages.push(`Thank you, ${names[i]}, for the ${event} gift!`)
+    )
+    return messages;
+}
+
+function countDown(integer) {
+    let number = integer;
+    while (number >= 0) {
+        console.log(number);
+        --number;
+    }
+}
